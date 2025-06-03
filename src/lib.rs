0f4966c439a69ec1c78e29293547b37b3a285f8b
Mika Cohen <mjc@kernel.org>
2025-06-03 15:22:04 -0600

    Fix tests for new header pieces

--- src/lib.rs
+++ src/lib.rs
@@ -11,5 +11,13 @@ pub struct Par2Header {
     pub md5: [u8; 16], // MD5 hash of the packet
     pub set_id: [u8; 16], // Unique identifier for the PAR2 set
     pub type_of_packet: [u8; 16], // Type of the packet
-    // Add more fields as per the PAR2 specification
+    pub creator: [u8; 16], // Creator of the PAR2 file
+    pub file_count: u32,  // Number of files in the PAR2 set
+    pub recovery_block_count: u32, // Number of recovery blocks
+    pub recovery_block_size: u64,  // Size of each recovery block
+    #[br(count = 256)] // Example: Adjust the count based on the PAR2 specification
+    pub file_description: Vec<u8>, // Description of the file
+
+    #[br(count = 512)] // Example: Adjust the count based on the PAR2 specification
+    pub packet_data: Vec<u8>, // Additional packet data
 }
