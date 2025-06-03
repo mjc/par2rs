8da63d424a0247f792f7ff6426fbc93d4c01d5d6
Mika Cohen <mjc@kernel.org>
2025-06-03 15:59:07 -0600

    Add packet definitions

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 pub mod args;
-pub mod repair;
 
 pub use args::parse_args;
 
@@ -21,4 +20,44 @@ pub struct Par2Header {
 
     #[br(count = 512)] // Example: Adjust the count based on the PAR2 specification
     pub packet_data: Vec<u8>, // Additional packet data
+}
+
+#[derive(Debug, BinRead)]
+pub struct MainPacket {
+    pub slice_size: u64, // Size of each slice
+    pub file_count: u32, // Number of files in the recovery set
+    #[br(count = 16)]
+    pub recovery_set_ids: Vec<[u8; 16]>, // File IDs of recovery set
+    #[br(count = 16)]
+    pub non_recovery_set_ids: Vec<[u8; 16]>, // File IDs of non-recovery set
+}
+
+#[derive(Debug, BinRead)]
+pub struct FileDescriptionPacket {
+    pub file_id: [u8; 16], // Unique identifier for the file
+    pub md5_hash: [u8; 16], // MD5 hash of the entire file
+    pub md5_16k: [u8; 16], // MD5 hash of the first 16kB of the file
+    pub file_length: u64, // Length of the file
+    #[br(count = 256)]
+    pub file_name: Vec<u8>, // Name of the file (not null-terminated)
+}
+
+#[derive(Debug, BinRead)]
+pub struct InputFileSliceChecksumPacket {
+    pub file_id: [u8; 16], // File ID of the file
+    #[br(count = 20)]
+    pub slice_checksums: Vec<( [u8; 16], u32 )>, // MD5 and CRC32 pairs for slices
+}
+
+#[derive(Debug, BinRead)]
+pub struct RecoverySlicePacket {
+    pub exponent: u32, // Exponent used to generate recovery data
+    #[br(count = 512)]
+    pub recovery_data: Vec<u8>, // Recovery data
+}
+
+#[derive(Debug, BinRead)]
+pub struct CreatorPacket {
+    #[br(count = 256)]
+    pub creator_info: Vec<u8>, // ASCII text identifying the client
 }
