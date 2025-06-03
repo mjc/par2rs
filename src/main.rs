b82aeac929bc45739599300ea3f942f47fde7882
Mika Cohen <mjc@kernel.org>
2025-06-03 15:10:12 -0600

    basic argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{Arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("par2rs")
+        .version("1.0")
+        .author("Your Name <your.email@example.com>")
+        .about("A Rust implementation of par2repair")
+        .arg(
+            Arg::new("input")
+                .help("Input file")
+                .required(true)
+                .value_parser(clap::value_parser!(String)),
+        )
+        .arg(
+            Arg::new("output")
+                .help("Output file")
+                .required(false)
+                .value_parser(clap::value_parser!(String)),
+        )
+        .get_matches();
+
+    if let Some(input) = matches.get_one::<String>("input") {
+        println!("Input file: {}", input);
+    }
+
+    if let Some(output) = matches.get_one::<String>("output") {
+        println!("Output file: {}", output);
+    }
 }
