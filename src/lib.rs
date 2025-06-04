d6b31e6991ae2d5b0c564a453ec0d14861f97c1e
Mika Cohen <mjc@kernel.org>
2025-06-04 12:08:41 -0600

    refactor the packet type stuff

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,21 @@ pub use packets::*; // Add this line to import all public items from packets mod
 
 pub fn parse_packets<R: std::io::Read + std::io::Seek>(reader: &mut R) -> Vec<Packet> {
     let mut packets = Vec::new();
+    let mut header = [0u8; 64];
 
-    while let Some(packet) = Packet::parse(reader, &[]) {
-        packets.push(packet);
+    while reader.read_exact(&mut header).is_ok() {
+        let type_of_packet = &header[48..64];
+
+        // Rewind the reader to the start of the packet
+        reader.seek(std::io::SeekFrom::Current(-64)).ok();
+
+        if let Some(packet) = Packet::from_bytes(type_of_packet, reader) {
+            packets.push(packet);
+        } else {
+            // Skip the rest of the packet if it cannot be parsed
+            let packet_length = u64::from_le_bytes(header[0..8].try_into().unwrap());
+            reader.seek(std::io::SeekFrom::Current(packet_length as i64 - 64)).ok();
+        }
     }
 
     packets
