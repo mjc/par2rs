f8c1b46f606c22fa1c67814a1868b61a8fd89449
Mika Cohen <mjc@kernel.org>
2025-06-04 12:34:11 -0600

    fix arg parsing to make sure it verifies paths

--- src/args.rs
+++ src/args.rs
@@ -1,21 +1,36 @@
 use clap::{Arg, Command};
+use std::fs;
 
 pub fn parse_args() -> clap::ArgMatches {
     Command::new("par2rs")
-        .version("1.0")
-        .author("Your Name <your.email@example.com>")
+        .version("0.1.0")
+        .author("Mika Cohen <mjc@kernel.org>")
         .about("A Rust implementation of par2repair")
         .arg(
             Arg::new("input")
                 .help("Input file")
                 .required(true)
-                .value_parser(clap::value_parser!(String)),
+                .value_parser(|input: &str| {
+                    let path = fs::canonicalize(input).map_err(|_| "Failed to resolve input path")?;
+                    if path.exists() {
+                        Ok(path.to_string_lossy().to_string())
+                    } else {
+                        Err(String::from("Input file does not exist"))
+                    }
+                }),
         )
         .arg(
             Arg::new("output")
                 .help("Output file")
                 .required(false)
-                .value_parser(clap::value_parser!(String)),
+                .value_parser(|output: &str| {
+                    let path = fs::canonicalize(output).map_err(|_| "Failed to resolve output path")?;
+                    if path.parent().map_or(true, |parent| parent.exists()) {
+                        Ok(path.to_string_lossy().to_string())
+                    } else {
+                        Err(String::from("Output directory does not exist"))
+                    }
+                }),
         )
         .get_matches()
 }
