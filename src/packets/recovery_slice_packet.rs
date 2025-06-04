a265d59c188049d5ee3ac044aba764a9f65c972b
Mika Cohen <mjc@kernel.org>
2025-06-04 17:45:00 -0600

    Fix bug in recoveryslicepacket

--- src/packets/recovery_slice_packet.rs
+++ src/packets/recovery_slice_packet.rs
@@ -10,7 +10,7 @@ pub struct RecoverySlicePacket {
     #[br(pad_after = 16)] // Skip the `type_of_packet` field
     pub set_id: [u8; 16], // Unique identifier for the PAR2 set
     pub exponent: u32, // Exponent used to generate recovery data
-    #[br(count = length as usize - (8 + 8 + 16 + 16 + 4))] // Subtract sizes of all other fields
+    #[br(count = length as usize - (8 + 8 + 16 + 16 + 16 + 4))] // Include the type_of_packet field (16 bytes) in the calculation
     pub recovery_data: Vec<u8>, // Recovery data
 }
 
