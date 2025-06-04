e835652f230eff743efcd5fdc4320e219927df85
Mika Cohen <mjc@kernel.org>
2025-06-03 20:05:31 -0600

    Fiddling

--- tests/par2_tests.rs
+++
@@ -1,9 +0,0 @@
-use std::path::Path;
-
-#[test]
-fn test_par2_header_parsing() {
-    let test_file_path = Path::new("test/fixtures/testfile.par2");
-    assert!(test_file_path.exists(), "Test file does not exist");
-
-    println!("Test file exists and is ready for further testing.");
-}
