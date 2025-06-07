9ba79de94e05fceeb4e5afc684ab97eaaeced616
Mika Cohen <mjc@kernel.org>
2025-06-06 20:18:41 -0600

    convert par2repair argument parsing to clap

--- src/args.rs
+++ src/args.rs
@@ -1,5 +1,6 @@
 use clap::{Arg, Command};
 use std::fs;
+use std::path::Path;
 
 pub fn parse_args() -> clap::ArgMatches {
     Command::new("par2rs")
@@ -33,6 +34,48 @@ pub fn parse_args() -> clap::ArgMatches {
                         Err(String::from("Output directory does not exist"))
                     }
                 }),
+        )
+        .get_matches()
+}
+
+pub fn parse_repair_args() -> clap::ArgMatches {
+    Command::new("par2repair")
+        .version("0.1.0")
+        .author("Mika Cohen <mjc@kernel.org>")
+        .about("A Rust implementation of par2 repair")
+        .arg(
+            Arg::new("par2_file")
+                .help("PAR2 file to use for repair")
+                .required(true)
+                .index(1)
+                .value_parser(|input: &str| {
+                    let path = Path::new(input);
+                    if path.exists() {
+                        Ok(input.to_string())
+                    } else {
+                        Err(format!("PAR2 file '{}' does not exist", input))
+                    }
+                }),
+        )
+        .arg(
+            Arg::new("files")
+                .help("Target files to repair (optional)")
+                .num_args(0..)
+                .index(2),
+        )
+        .arg(
+            Arg::new("verbose")
+                .help("Verbose output")
+                .short('v')
+                .long("verbose")
+                .action(clap::ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("quiet")
+                .help("Quiet output (errors only)")
+                .short('q')
+                .long("quiet")
+                .action(clap::ArgAction::SetTrue),
         )
         .get_matches()
 }
