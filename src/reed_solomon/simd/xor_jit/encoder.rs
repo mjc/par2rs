654e80ae0b4ebcee30afc0bedcc526c9587c7b76
Mika Cohen <mjc@kernel.org>
2026-04-25 08:36:28 -0600

    Refactor AVX2 encoder operands

--- src/reed_solomon/simd/xor_jit/encoder.rs
+++ src/reed_solomon/simd/xor_jit/encoder.rs
@@ -21,22 +21,35 @@ impl Program {
     }
 
     pub fn vmovdqu_ymm0_from_rdi(mut self) -> Self {
-        self.instructions.push(Instruction::VmovdquYmm0FromRdi);
+        self.instructions.push(Instruction::VmovdquLoad {
+            dst: Ymm::Ymm0,
+            base: BaseReg::Rdi,
+        });
         self
     }
 
     pub fn vmovdqu_ymm1_from_rsi(mut self) -> Self {
-        self.instructions.push(Instruction::VmovdquYmm1FromRsi);
+        self.instructions.push(Instruction::VmovdquLoad {
+            dst: Ymm::Ymm1,
+            base: BaseReg::Rsi,
+        });
         self
     }
 
     pub fn vpxor_ymm0_ymm0_ymm1(mut self) -> Self {
-        self.instructions.push(Instruction::VpxorYmm0Ymm0Ymm1);
+        self.instructions.push(Instruction::Vpxor {
+            dst: Ymm::Ymm0,
+            lhs: Ymm::Ymm0,
+            rhs: Ymm::Ymm1,
+        });
         self
     }
 
     pub fn vmovdqu_rsi_from_ymm0(mut self) -> Self {
-        self.instructions.push(Instruction::VmovdquRsiFromYmm0);
+        self.instructions.push(Instruction::VmovdquStore {
+            base: BaseReg::Rsi,
+            src: Ymm::Ymm0,
+        });
         self
     }
 
@@ -56,10 +69,9 @@ impl Program {
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Instruction {
     MovEaxImm32(u32),
-    VmovdquYmm0FromRdi,
-    VmovdquYmm1FromRsi,
-    VpxorYmm0Ymm0Ymm1,
-    VmovdquRsiFromYmm0,
+    VmovdquLoad { dst: Ymm, base: BaseReg },
+    VmovdquStore { base: BaseReg, src: Ymm },
+    Vpxor { dst: Ymm, lhs: Ymm, rhs: Ymm },
     Vzeroupper,
     Ret,
 }
@@ -68,12 +80,56 @@ impl Instruction {
     fn encode(self) -> Vec<u8> {
         match self {
             Self::MovEaxImm32(value) => [vec![0xb8], value.to_le_bytes().to_vec()].concat(),
-            Self::VmovdquYmm0FromRdi => vec![0xc5, 0xfe, 0x6f, 0x07],
-            Self::VmovdquYmm1FromRsi => vec![0xc5, 0xfe, 0x6f, 0x0e],
-            Self::VpxorYmm0Ymm0Ymm1 => vec![0xc5, 0xfd, 0xef, 0xc1],
-            Self::VmovdquRsiFromYmm0 => vec![0xc5, 0xfe, 0x7f, 0x06],
+            Self::VmovdquLoad { dst, base } => {
+                vec![0xc5, 0xfe, 0x6f, modrm_memory(dst.code(), base.code())]
+            }
+            Self::VmovdquStore { base, src } => {
+                vec![0xc5, 0xfe, 0x7f, modrm_memory(src.code(), base.code())]
+            }
+            Self::Vpxor { dst, lhs, rhs } => {
+                debug_assert_eq!(dst, lhs);
+                vec![0xc5, 0xfd, 0xef, modrm_register(dst.code(), rhs.code())]
+            }
             Self::Vzeroupper => vec![0xc5, 0xf8, 0x77],
             Self::Ret => vec![0xc3],
         }
     }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Ymm {
+    Ymm0,
+    Ymm1,
+}
+
+impl Ymm {
+    const fn code(self) -> u8 {
+        match self {
+            Self::Ymm0 => 0,
+            Self::Ymm1 => 1,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum BaseReg {
+    Rdi,
+    Rsi,
+}
+
+impl BaseReg {
+    const fn code(self) -> u8 {
+        match self {
+            Self::Rsi => 6,
+            Self::Rdi => 7,
+        }
+    }
+}
+
+const fn modrm_memory(reg: u8, rm: u8) -> u8 {
+    (reg << 3) | rm
+}
+
+const fn modrm_register(reg: u8, rm: u8) -> u8 {
+    0xc0 | (reg << 3) | rm
 }
