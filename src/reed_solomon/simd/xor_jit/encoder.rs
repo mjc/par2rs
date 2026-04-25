48156a81ab05c8d789b8273e96c234447d0eef75
Mika Cohen <mjc@kernel.org>
2026-04-25 08:34:21 -0600

    Refactor XOR JIT encoder around instructions

--- src/reed_solomon/simd/xor_jit/encoder.rs
+++ src/reed_solomon/simd/xor_jit/encoder.rs
@@ -2,7 +2,7 @@
 
 #[derive(Debug, Default)]
 pub struct Program {
-    bytes: Vec<u8>,
+    instructions: Vec<Instruction>,
 }
 
 impl Program {
@@ -11,17 +11,34 @@ impl Program {
     }
 
     pub fn mov_eax_imm32(mut self, value: u32) -> Self {
-        self.bytes.push(0xb8);
-        self.bytes.extend_from_slice(&value.to_le_bytes());
+        self.instructions.push(Instruction::MovEaxImm32(value));
         self
     }
 
     pub fn ret(mut self) -> Self {
-        self.bytes.push(0xc3);
+        self.instructions.push(Instruction::Ret);
         self
     }
 
     pub fn finish(self) -> Vec<u8> {
-        self.bytes
+        self.instructions
+            .into_iter()
+            .flat_map(Instruction::encode)
+            .collect()
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Instruction {
+    MovEaxImm32(u32),
+    Ret,
+}
+
+impl Instruction {
+    fn encode(self) -> Vec<u8> {
+        match self {
+            Self::MovEaxImm32(value) => [vec![0xb8], value.to_le_bytes().to_vec()].concat(),
+            Self::Ret => vec![0xc3],
+        }
     }
 }
